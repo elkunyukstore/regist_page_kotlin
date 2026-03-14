b9128ecce8f0d065755f99ed1c06a467163b32e3
elkunyukstore <tes546113@gmail.com>
2026-03-14 13:02:32 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,15 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
